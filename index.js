ca78828337f8fc972a9c069c4fd1ecbc16994236
Nataniel4 <nataniel.lopez@fizzmod.com>
2019-07-26 18:21:27 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const { ElasticSearch } = require('./lib');
+
+module.exports = ElasticSearch;
---
+++ tests/elasticsearch-test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const assert = require('assert');
+
+const sandbox = require('sinon').createSandbox();
+
+const ElasticSearch = require('./../index');
+
+const ElasticSearchError = require('./../lib/elasticsearch-error');
+
+describe('ElasticSearch', () => {
+
+	// your tests here...
+
+});
