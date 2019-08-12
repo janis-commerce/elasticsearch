845c7df36116179626db926298adae97df0cd8fb
Nataniel4 <nataniel.lopez@fizzmod.com>
2019-08-12 10:55:40 -0300

    JSDoc and minor code cleanup + unit tests

--- tests/elasticsearch-filters-test.js
+++ tests/elasticsearch-filters-test.js
@@ -2,8 +2,6 @@
 
 const assert = require('assert');
 
-const sandbox = require('sinon').createSandbox();
-
 const ElasticSearchFilters = require('./../lib/elasticsearch-filters');
 
 describe('ElasticSearchFilters', () => {
@@ -73,7 +71,91 @@ describe('ElasticSearchFilters', () => {
 			});
 		});
 
+		it('should return the elasticsearch query (mixed)', () => {
 
+			const mixedFilters = {
+				field: 'value',
+				$eq: {
+					field: 'value'
+				},
+				$ne: {
+					field: 'value'
+				},
+				$in: {
+					field: ['value']
+				},
+				$nin: {
+					field: ['value']
+				},
+				$gt: {
+					id: 1
+				},
+				$gte: {
+					id: 1
+				},
+				$lt: {
+					id: 1
+				},
+				$lte: {
+					id: 1
+				}
+			};
+
+			assert.deepStrictEqual(ElasticSearchFilters.getFilters(mixedFilters), {
+				query: {
+					bool: {
+						must: [
+							{ term: { 'field.raw': 'value' } },	{ term: { field: 'value' } },
+							{
+								range: {
+									id: {
+										gt: 1,
+										gte: 1,
+										lt: 1,
+										lte: 1
+									}
+								}
+							}
+						],
+						must_not: [
+							{ term: { 'field.raw': 'value' } }, { term: { field: 'value' } }
+						]
+					}
+				}
+			});
+		});
+
+		it('should ignore the $in and $nin terms when the values is not an array', async () => {
+
+			const badFilters = {
+				$in: 'not-array',
+				$nin: 'not-array'
+			};
+
+			assert.deepStrictEqual(ElasticSearchFilters.getFilters(badFilters), {
+				query: {
+					bool: {
+						must: [],
+						must_not: []
+					}
+				}
+			});
+
+		});
+
+		context('when the recieved filters are invalid', () => {
+
+			it('should return an empty object when whe filters not exists', async () => {
+				assert.deepStrictEqual(ElasticSearchFilters.getFilters(), {});
+			});
+
+			it('should return an empty object when whe filters is not an object', async () => {
+				assert.deepStrictEqual(ElasticSearchFilters.getFilters('filters'), {});
+			});
+
+			it('should return an empty object when whe filters is an array', async () => {
+				assert.deepStrictEqual(ElasticSearchFilters.getFilters(['filters']), {});
+			});
+		});
 	});
-
 });
